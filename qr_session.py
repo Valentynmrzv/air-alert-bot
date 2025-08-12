8f70cc3f25e22fc085640683b9cb5bb70c2bf02f
Valentin Mrzv <131597869+Valentynmrzv@users.noreply.github.com>
2025-08-12 08:22:40 +0300

    11

--- qr_session.py
+++ qr_session.py
@@ -1,24 +1,12 @@
-# qr_session.py
-import os
-import asyncio
-from telethon import TelegramClient
+from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from dotenv import load_dotenv
 
-load_dotenv()
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
+API_ID = 27661955
+API_HASH = "f24f00c4e7aee89b1b1bb53a56fd297a"
+SESSION_FILE = "/home/vlntnmrzv/air-alert-bot/session.session"
 
-async def main():
-    async with TelegramClient(StringSession(), api_id, api_hash) as client:
-        print("🔳 Зараз з'явиться QR. Відкрий Telegram на телефоні:")
-        print("  Налаштування → Пристрої → Підключити пристрій → Скануй QR")
-        qr = await client.qr_login()
-        print(qr.url)  # якщо TTY не малює ASCII-QR, просто відкрий цей URL з камери/браузера Telegram
-        await qr.wait()  # чекаємо поки ти відскануєш і підтвердиш
-        print("\n=== TELETHON_SESSION ===")
-        print(client.session.save())
-        print("========================\n")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+with TelegramClient(SESSION_FILE, API_ID, API_HASH) as client:
+    string = StringSession.save(client.session)
+    print("\n=== TELETHON_SESSION ===")
+    print(string)
+    print("========================\n")
